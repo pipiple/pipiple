a5c1d4420d326ab0a59eb2a3af54f45341ad48f4
shinji <46948336+pipiple@users.noreply.github.com>
2019-05-24 01:35:59 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
       :port => 587,
       :domain => 'smtp.gmail.com',
       :user_name => 'pipiple.official@gmail.com',
-      :password => "datepipi03",
+      :password => "※",
       :authentication => 'login',
   }
 
